aa7b16316151ff181a0aa478e666062045cfa7c9
djordje <djordjesteva@gmail.com>
2019-01-22 01:10:00 +0100

    Updates

---
+++ precache-manifest.b5a73ed57f04c687ac047bd32d65a2a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "77385ac92a2f5fe7e91e",
+    "url": "/tic-tac-toe/static/js/main.77385ac9.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/tic-tac-toe/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "77385ac92a2f5fe7e91e",
+    "url": "/tic-tac-toe/static/css/main.ae848608.chunk.css"
+  },
+  {
+    "revision": "d182cf05ab6e106efc2dd97aadc4d8a1",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
